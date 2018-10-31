7da8ea08433d0a2df59275da2a7d9f17f739970c
toral <tj244@njit.edu>
2018-10-31 14:14:58 -0400

    Task: Created controller,route and view to show contact page

--- routes/web.php
+++ routes/web.php
@@ -19,5 +19,6 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/about', 'PagesController@about')->name('about');
+Route::get('/contact', 'PagesController@contact')->name('contact');
 
 

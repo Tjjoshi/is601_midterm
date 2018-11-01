8f317f48a52b90e38d905b7ca1cd07b735033e4b
toral <tj244@njit.edu>
2018-11-01 18:05:30 -0400

    Task:wrote a unit test to delete a car

--- tests/Unit/DeleteUserTest.php
+++ tests/Unit/DeleteUserTest.php
@@ -11,6 +11,7 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 class DeleteUserTest extends TestCase
 {
     public function testDeleteUser(){
+        $user = User::find(51);
         DB::table('users')->where('id', '>', 50)->delete();
         $user = User::all();
         $this->assertDatabaseMissing('users',['id' => '51']);
